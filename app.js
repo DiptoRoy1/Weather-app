6d609deb98435a92dddef638be38e9f671d5886d
Dipto Roy <fordipto2020@gmail.com>
2024-12-04 23:24:02 +0600

    Weather app version 1.0 release

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
-const btn = document.getElementById("btn");
-let body = document.querySelector("body");
-let mode = "light";
-btn.addEventListener("click", () => {
-    if (mode === "light") {
-        body.style.backgroundColor = "black";
-        document.querySelector("p").style.color = "white";
-        mode = "dark";
+const apiKey = "0121a80939fba8842276a618810ad2fe";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const inputCity = document.querySelector("input");
+const searchButton = document.querySelector("button");
+const weathericone = document.querySelector(".weather-icone");
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + `${city}&appid=${apiKey}`);
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+
+    console.log(data);
+
+    if (data.weather[0].main == "Clouds") {
+        weathericone.src = "images/clouds.png";
     }
-    else {
-        body.style.backgroundColor = "White";
-        document.querySelector("p").style.color = "black";
-        mode = "light";
+    if (data.weather[0].main == "clear") {
+        weathericone.src = "images/clear.png";
     }
-})
+    if (data.weather[0].main == "drizzle") {
+        weathericone.src = "images/drizzle.png";
+    }
+    if (data.weather[0].main == "mist") {
+        mist
+        weathericone.src = "images/mist.png";
+    }
+    if (data.weather[0].main == "rain") {
+        weathericone.src = "images/rain.png";
+    }
+    if (data.weather[0].main == "wind") {
+        weathericone.src = "images/wind.png";
+    }
+    if (data.weather[0].main == "Haze") {
+        weathericone.src = "images/haze.png";
+    }
+}
+
+
+
+searchButton.addEventListener("click", () => {
+    checkWeather(inputCity.value);
+});
+
+inputCity.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        checkWeather(inputCity.value);
+    }
+});
